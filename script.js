f9e7db4359a99b38e4b8ba19515b046e58bc2df8
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-24 14:07:49 +0000

    create getHumanChoice function in script.js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function getComputerChoice() {
     return ch[r % 3];
 }
 
+function getHumanChoice(){
+    let ch = prompt("Enter your choice: Rock, Paper, or Scissors");
+    if (ch === null) {
+        alert("EMPTY CHOICE");
+    }
+    ch = (ch.trim()).toLowerCase();
+    if(ch === "rock" || ch === "paper" || ch === "scissors") {
+        return ch.charAt(0).toUpperCase() + ch.slice(1);
+    }
+    else {        
+        alert("Invalid choice. Please enter Rock, Paper, or Scissors.");
+        return getHumanChoice();
+    }
+}
