497bd58be149ab4e10fb602c13003277951ce288
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-07-24 14:02:30 +0000

    create and link script.js file

---
+++ script.js
@@ -0,0 +1,5 @@
+//Functions:
+// playRound: Function to play a single round of Rock, Paper, Scissors
+// getScore: Returns the result of the round
+// getComputerChoice: Function to randomly select Rock, Paper, or Scissors
+// getHumanChoice: Function to get the user's choice
