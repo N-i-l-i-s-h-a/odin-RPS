3236ce3fe684be2a5a855a5960fb79c406183d17
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-08-08 10:59:46 +0000

    clean js file

--- script.js
+++ script.js
@@ -1,86 +1,38 @@
-//Functions:
-// playRound: Function to play a single round of Rock, Paper, Scissors
-// getResults: Returns the result of the round
-// getComputerChoice: Function to randomly select Rock, Paper, or Scissors
-// getHumanChoice: Function to get the user's choice
-// updateScore: Function to update the scores based on the choices made
+//onStart() : to initialize scores to zero
+//displayScore() : fired when start is clicked
+//updateScore() : updates the scoreboard according to humanChoice and machiineChoice, fired when any button is clicked
+//getComputerChoice() : 
+// random generator function
 
-function getComputerChoice() {
+var scoreH, scoreM;
+
+function onStart(){
+    scoreH = 0;
+    scoreM = 0;
+    playRound();
+}
+
+function getComputerChoice(){
     const ch = ['rock', 'paper', 'scissors'];
     const r = Math.floor(Math.random() * 10);
     return ch[r % 3];
 }
 
-function updateScore(hum, com){
-    if(hum === com){
-        scoreC++;
-        scoreH++;
-    }
-    else if(hum === "rock"){
-        if(com === "scissors"){
-            scoreH++;
-        }
-        else{
-            scoreC++; 
-        }
-    }
-    else if(hum === "paper"){
-        if(com === "rock"){
-            scoreH++;
-        }
-        else{
-            scoreC++;
-        }
-    }
-    else{
-        if(com === "paper"){
-            scoreH++;
-        }
-        else{
-            scoreC++;
-        }
-    }
-}
-
-function onStart(){
-    scoreH = 0;
-    scoreC = 0;
-    let mid = document.querySelector("#middle");
-    mid.innerText = "";
-    let scoreBoard = document.createElement("div");
-    let human = document.createElement("div");
-    let machine = document.createElement("div");
-    scoreBoard.innerText = "SCORE";
-    human.innerText = `Human : ${scoreH}`;
-    machine.innerText = `Computer : ${scoreC}`;
-    scoreBoard.style.cssText = "font-size: 55px; display: flex; flex-direction: column; justify-content: space-evenly; align-items: center; border: 2px dashed black; border-radius: 20%; height: 400px; width: 500px;";
-    human.setAttribute("style", "font-size: 45px;");
-    machine.setAttribute("style", "font-size: 45px;");
-    scoreBoard.appendChild(human);
-    scoreBoard.appendChild(machine);
-    mid.appendChild(scoreBoard);
-}
-
 function playRound(){
-    const hum = document.querySelector(".btn").addEventListener('click', function(e){ return e.target.id; });
-    const com = getComputerChoice();
-    updateScore(hum, com, scoreH, scoreC);
-    onStart();
+    displayScore();
 }
 
-function getResults() {
-    var diff = scoreH - scoreC;
-    if(diff === 0){
-        console.log("It's a tie!");
-    }
-    else if(diff > 0){
-        console.log(`You win by ${diff} ${diff > 1 ? "points" : "point"}!`);
-    }
-    else{
-        console.log(`You lose by ${Math.abs(diff)} ${diff < -1 ? "points" : "point"}.`);
-    }
-    return prompt("Do you want to play again? (yes/no)", "yes") === "yes" ? playRound() : null; 
+function displayScore(){
+    const mid = document.querySelector("#middle");
+    mid.innerText = "";
+    const scoreBoard = document.createElement("div");
+    const score = document.createElement("div");
+    const human = document.createElement("div");
+    const machine = document.createElement("div");
+    score.innerText = "SCORE";
+    human.innerText = `Human : ${scoreH}`;
+    machine.innnerText = `Machine : ${scoreM}`;
+    scoreBoard.style.cssText = "width: 300px; height: 400px; padding: 16px; display: flex; flex-direction: column; justify-content: center; align-items: space-evenly;";
+    score.style.cssText = "font-size: 55px;";
+    return;
 }
-
-//The game starts here
-var scoreC, scoreH;
