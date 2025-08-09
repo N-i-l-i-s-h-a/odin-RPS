97a66c70f0e3de1525e6c794744ee5316be5e82c
N-i-l-i-s-h-a <115420591+N-i-l-i-s-h-a@users.noreply.github.com>
2025-08-09 15:51:52 +0000

    implement playRound() and handle event using QuerySelectorAll()

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // random generator function
 
 var scoreH, scoreM;
+var hum, com;
 
 function onStart(){
     scoreH = 0;
@@ -18,21 +19,55 @@ function getComputerChoice(){
     return ch[r % 3];
 }
 
+function getChoice(e){
+    hum = e.target.id;
+}
+
 function playRound(){
     displayScore();
+    const list = document.querySelectorAll(".btn");
+    for(let i = 0; i < 5; i++){  
+        list.forEach(function(btn){
+            btn.addEventListener('click', () => getChoice(e));
+        });
+        com = getComputerChoice();
+        if(com === "rock" && hum == "paper")
+            scoreH++;
+        else if(com === "rock" && hum === "scissors")
+            scoreM++;
+        else if(com === "paper" && hum === "scissors")
+            scoreH++;
+        else if(com === "paper" && hum === "rock")
+            scoreM++;
+        else if(com === "scissors" && hum === "rock")
+            scoreH++;
+        else if(com === "scissors" && hum === "paper")
+            scoreM++;
+        else{
+            scoreM++;
+            scoreH++;
+        }
+        displayScore();
+    }
 }
 
 function displayScore(){
     const mid = document.querySelector("#middle");
     mid.innerText = "";
-    const scoreBoard = document.createElement("div");
-    const score = document.createElement("div");
-    const human = document.createElement("div");
-    const machine = document.createElement("div");
+    var scoreBoard = document.createElement("div");
+    var score = document.createElement("div");
+    var human = document.createElement("div");
+    var machine = document.createElement("div");
     score.innerText = "SCORE";
     human.innerText = `Human : ${scoreH}`;
-    machine.innnerText = `Machine : ${scoreM}`;
-    scoreBoard.style.cssText = "width: 300px; height: 400px; padding: 16px; display: flex; flex-direction: column; justify-content: center; align-items: space-evenly;";
+    machine.innerText = `Machine : ${scoreM}`;
+    scoreBoard.appendChild(score);
+    scoreBoard.appendChild(human);
+    scoreBoard.appendChild(machine);
+    scoreBoard.style.cssText = "width: 300px; height: 300px; background-color: bisque; padding: 0px 16px 0px 16px; border: 2px dashed rgba(0, 0, 0, 0.507); border-radius: 10%; display: flex; flex-direction: column; justify-content: space-evenly; align-items: center;";
     score.style.cssText = "font-size: 55px;";
+    human.style.cssText = "font-size: 45px;";
+    machine.style.cssText = "font-size: 45px;";
+    mid.appendChild(scoreBoard);
     return;
 }
